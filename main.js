0b3a8e3b8ef8fa605777479cd26b6b6808cdf105
ruru-dev <rurutia89@gmail.com>
2023-04-02 23:41:18 -0500

    design rock paper scissors game

--- main.js
+++ main.js
@@ -12,12 +12,38 @@ const rl = readline.createInterface({
 });
 
 // the function that will be called by the unit test below
+
 const rockPaperScissors = (hand1, hand2) => {
+  hand1 = hand1.trim().toLocaleLowerCase();
+  hand2 = hand2.trim().toLocaleLowerCase();
 
   // Write code here
-  // Use the unit test to see what is expected
+  if (hand1 === hand2) {
+    return "It's a tie!"
+  }
 
+  if (hand1 === "paper" && hand2 === "rock") {
+    return "Hand one wins!"
+  }
+
+  else if (hand1 === "rock" && hand2 === "scissors") {
+    return "Hand one wins!"
+  }
+
+  else if (hand1 === 'scissors' && hand2 === 'paper') {
+    return "Hand one wins!"
+  }
+
+  else if (hand1 === 'scissors' && hand2 === 'paper') {
+    return "Hand one wins!"
+  }
+
+  else {
+    return "Hand two wins!"
+  }
 }
+
+// Use the unit test to see what is expected
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
